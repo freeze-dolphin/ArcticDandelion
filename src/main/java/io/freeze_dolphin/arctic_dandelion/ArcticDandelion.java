471d474763b7dcefdcc342658adc6a8f1a293e5a
freeze-dolphin <40729044+freeze-dolphin@users.noreply.github.com>
2021-01-17 15:54:21 +0800

    .

---
+++ src/main/java/io/freeze_dolphin/arctic_dandelion/ArcticDandelion.java
@@ -0,0 +1,12 @@
+package io.freeze_dolphin.arctic_dandelion;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class ArcticDandelion extends JavaPlugin {
+	
+	@Override
+	public void onEnable() {
+		
+	}
+	
+}
